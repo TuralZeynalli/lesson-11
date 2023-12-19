74527252fd48fa00fb704b663bc1967f164e51b9
Tural <tural176@mail.ru>
2023-12-19 18:09:28 +0400

    lesson 11

---
+++ LetterVsSentence/LetterVsSentence/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LetterVsSentence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = LetterVsSentences(" ou");
+            Console.WriteLine(result);
+        }
+
+        static string LetterVsSentences (string word)
+        {
+        string sentence = "You can't kill me, cause i'm already inside you...";
+
+            if (sentence.Contains(word.Trim()))
+            {
+                return "this word included in this sentences...";
+            }
+            else
+            {
+                return "this word isn't included in this sentences...";
+            }
+        }
+/*
+        static void Sdajsk() 
+        
+        {
+            string name = "hahahahuhehassshu asdasd asdasd";
+            string[] surname = name.Split('u');
+
+            foreach (var item in surname)
+            {
+                Console.WriteLine(item);
+            }
+        
+        }*/
+        
+        
+    }
+}
---
+++ backwards/backwards/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace backwards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool sameOrDifferent = BackWards("");
+            Console.WriteLine(sameOrDifferent);
+        }
+
+        static bool BackWards(string word)
+
+        {
+            char[] letters = word.ToCharArray();
+
+            Array.Reverse(letters);
+
+            string opposite = new string(letters);
+
+            if (opposite == word)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
